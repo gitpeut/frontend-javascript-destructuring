e04d913de0725b4cdd07c75db6729173496e6eaf
gitpeut <peut@peut.org>
2021-06-20 01:16:14 +0200

    done

--- destructuring.js
+++ destructuring.js
@@ -7,6 +7,10 @@ const product = {
   screenType: 'LED',
 }
 
+let {price, refreshRate } = product;
+
+console.log( 'price ' + price + ' refreshRate ' + refreshRate);
+
 // Opdracht 2: Destructure type en brand uit onderstaand object, en log ze daarna in de terminal
 
 const productInformation = {
@@ -18,6 +22,10 @@ const productInformation = {
   screenQuality: 'Ultra HD/4K',
   smartTv: true,
 }
+
+let {type,brand} = productInformation.general;
+
+console.log( 'type ' + type + ' brand ' + brand);
 
 // Opdracht 3: destructure wifi en bluetooth uit onderstaand object, en log ze daarna in de terminal
 
@@ -39,14 +47,24 @@ const tvOptions = {
   },
 };
 
+let { wifi, bluetooth } = tvOptions.options.connectivity.popular;
+
+console.log( (wifi?'has wifi':' has no wifi') + (bluetooth?' has bluetooth':' has no bluetooth') );
+
 // Opdracht 4: Destructure beide waardes uit onderstaande array, en log ze daarna in de terminal
 
 const connectivity = ['wifi', 'bluetooth'];
 
+let [conWifi,conBluetooth]  = connectivity;
+
+console.log( conWifi + ' ' + conBluetooth);
 
 // Opdracht 5: Destructure de >eerste< en >derde< waarde uit onderstaande array, en log ze daarna in de terminal
 
 const tvSizes = [41, 43, 46, 56, 65];
+
+let [first,,third] = tvSizes;
+console.log( first + ' ' + third);
 
 
 // opdracht 6: Destructure name en adress uit de return value van deze functie en log ze daarna in de terminal
@@ -58,8 +76,14 @@ function getCompanyDetails() {
   }
 }
 
+let { name: compName, adress : compAddress } = getCompanyDetails();
+console.log( compName + ' ' + compAddress);
+
 // opdracht 7: Destructure beide zinnetjes uit de return value van de getDetails functie en log ze in de terminal. Let op: de functie verwacht argumenten bij het aanroepen!
 
 function getDetails(name, age) {
   return [`Your name is ${name}`, `You are ${age} years old`];
 }
+
+let [ detName, detAge ] = getDetails( "Peut", 62);
+console.log( detName + ', ' + detAge );
